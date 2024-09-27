fb4d2b6fec4aefb7abffcc903160b55c0fca2c2b
231130651 <michelle.anditio@students.mikroskil.ac.id>
2024-09-27 17:04:36 +0700

    unfinished

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../organisms/Header"
+import ProductSection from "../organisms/ProductSection"
+
+function Home = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <ProductSection />
+            </main>
+        </>
+    )
+}
+
+export default Home
