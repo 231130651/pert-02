2cb400fd2fc4d1d9f779d96449ea6b1f2663e161
231130651 <michelle.anditio@students.mikroskil.ac.id>
2024-10-01 23:31:14 +0700

    M03 index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
+
 import App from './M03App';
-=======
-import App from './M03App';
-import reportWebVitals from './reportWebVitals';
->>>>>>> 523231e489d077f7e7677069fb8428a75a555d1c
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
